1750fe1f55dd62d310c4edc8cd745068f1f5ca15
William Speirs <wspeirs@datto.com>
2020-01-28 09:29:10 -0500

    Added a bunch of documentation and a few future methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+//! mem_table is an in-memory table of data, modeled after [Pandas](https://pandas.pydata.org/) for Python.
 #[macro_use]
 extern crate log;
 
@@ -16,16 +17,25 @@ mod row_table;
 pub use crate::row_table::RowTable;
 pub use crate::value::Value;
 
-///
 /// The main interface into the mem_table library
-///
 pub trait Table {
+    /// Create a blank Table given the column names
+    fn new(columns :&[&str]) -> Self;
+
+    /// Read in a CSV file, and construct a Table
+    fn from_csv<P: AsRef<Path>>(path: P) -> Result<Self, IOError>;
+
     fn iter(&self) -> RowIter;
     fn into_iter(self) -> RowIntoIter;
 //    fn row_mut_iter(&mut self) -> RowMutIter;
 
+    /// Return the list of column names
     fn columns(&self) -> &Vec<String>;
+
+    /// Returns the length (number of rows) of the Table.
     fn len(&self) -> usize;
+
+    /// Returns the width (length of column list) of the Table.
     fn width(&self) -> usize;
 
     // iterators that only return some of the columns
@@ -34,7 +44,12 @@ pub trait Table {
 //    fn col_into_iter(self, cols :&Vec<&str>) -> ColIntoIter;
 //    fn col_mut_iter(&mut self, cols :&Vec<&str>) -> ColMutIter;
 
+    /// Groups values that are the same together, providing a `TableSlice` for each unique value found.
+    /// Note: You cannot hash or compare properly `f64`, so any `f64` value is wrapped in [`OrderedFloat`](https://docs.rs/ordered-float/*/ordered_float/).
     fn group_by(&self, column :&str) -> Result<HashMap<&Value, TableSlice<Self>>, TableError> where Self: Sized;
+
+    /// Returns the unique values for a given column
+    /// Note: You cannot hash or compare properly `f64`, so any `f64` value is wrapped in [`OrderedFloat`](https://docs.rs/ordered-float/*/ordered_float/).
     fn unique(&self, column :&str) -> Result<HashSet<&Value>, TableError>;
 
     fn append(&mut self, table :impl Table) -> Result<(), TableError>;
@@ -51,6 +66,17 @@ pub trait Table {
     fn find(&self, column :&str, value :&Value) -> Result<TableSlice<Self>, TableError> where Self: Sized;
     fn find_by<P: FnMut(&Vec<Value>) -> bool>(&self, predicate :P) -> Result<TableSlice<Self>, TableError> where Self: Sized;
 
+    /// Sorts the rows in the table, in an unstable way, in ascending order, by the columns provided, in the order they're provided.
+    ///
+    /// If the columns passed are `A`, `B`, `C`, then the rows will be sored by column `A` first, then `B`, then `C`.
+    fn sort(&mut self, columns :&[&str]) -> Result<(), TableError> { unimplemented!(); }
+    fn sort_by<F: FnMut(&Vec<Value>, &Vec<Value>) -> Ordering>(&mut self, columns :&[&str], compare :F) { unimplemented!(); }
+    fn stable_sort(&mut self, columns :&[&str]) -> Result<(), TableError> { unimplemented!(); }
+    fn stable_sort_by<F: FnMut(&Vec<Value>, &Vec<Value>) -> Ordering>(&mut self, compare :F) -> Result<(), TableError> { unimplemented!(); }
+
+    fn split_rows_at<T: Table>(&self, mid :usize) -> (TableSlice<T>, TableSlice<T>) { unimplemented!(); }
+
+    /// Writes the data out to a CSV file
     fn to_csv(&self, csv_path :&Path) -> Result<(), TableError>;
 }
 
@@ -138,6 +164,7 @@ use std::hash::{Hash, Hasher};
 use std::cell::Ref;
 use rayon::iter::ParallelExtend;
 use rayon::prelude::IntoParallelIterator;
+use std::cmp::Ordering;
 
 #[cfg(test)] static LOGGER_INIT: Once = Once::new();
 
--- src/value.rs
+++ src/value.rs
@@ -4,9 +4,7 @@ use dtparse::parse;
 use ordered_float::OrderedFloat;
 
 
-///
-/// Various types of values found in the cells of a Table
-///
+/// Various types of values found in the cells of a [`Table`](trait.Table.html)
 #[derive(Debug, Clone, Hash, Ord, PartialOrd, Eq, PartialEq)]
 pub enum Value {
     String(String),
@@ -17,6 +15,16 @@ pub enum Value {
 }
 
 impl Value {
+    /// Constructs a new [`Value`] from a `&str`.
+    ///
+    /// The method constructs a [`Value`] using the following:
+    /// 1. checks to see if the string is empty, then constructs `Value::Empty`
+    /// 1. if the string contains `-`, `/`, or `:`, then attempts to parse as a [`DateTime`](#https://docs.rs/chrono/*/chrono/struct.DateTime.html)
+    /// 1. if the string contains `.`, then attempts to parse as a `f64`
+    /// 1. if the string can be parsed as a `i64`, then a `Value::Integer` is constructed
+    /// 1. finally a `Value::String` is constructed using the string
+    ///
+    /// [`Value`]: enum.Value.html
     pub fn new(value :&str) -> Value {
         // first check to see if it's empty
         if value.is_empty() {
